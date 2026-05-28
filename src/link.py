2f986abcaf162592b11a9ca6cf3b410f62ec4efa
Stoupy51 <stoupy51@gmail.com>
2026-05-28 21:49:47 +0200

    chore: 🔧 `@e[...,limit=1]` to `@n[...]`

--- src/link.py
+++ src/link.py
@@ -152,11 +152,11 @@ execute store result score #length {ns}.data run data get storage {ns}:main id
 
     on_item_content += f"""
 # Get rotation looking at the origin of the explosion and kill the item entity
-execute at @s run tp @s ~ ~10 ~ facing entity @e[type=marker,tag=realistic_explosion.origin,limit=1] feet
+execute at @s run tp @s ~ ~10 ~ facing entity @n[type=marker,tag=realistic_explosion.origin] feet
 execute at @s run tp @s ~ ~-10 ~
 
 # Copy the rotation to the falling block and kill the item entity
-data modify entity @e[type=falling_block,tag=realistic_explosion.new,sort=nearest,distance=..1,limit=1] Rotation set from entity @s Rotation
+data modify entity @n[type=falling_block,tag=realistic_explosion.new,sort=nearest,distance=..1] Rotation set from entity @s Rotation
 kill @s
 
 # Schedule the function that applies the explosion motion
